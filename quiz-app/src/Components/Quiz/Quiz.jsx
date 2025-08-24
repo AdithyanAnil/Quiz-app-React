085fc13b139db6ea6124f557a9b10b24871df60f
Adithyan S Anil <ad7.adithyan@gmail.com>
2025-08-24 10:49:14 +0530

    Qestion 1 demo template  added

--- quiz-app/src/Components/Quiz/Quiz.jsx
+++ quiz-app/src/Components/Quiz/Quiz.jsx
@@ -3,7 +3,19 @@ import './Quiz.css'
 
 const Quiz = () => {
   return (
-    <div>Quiz</div>
+    <div className="container">
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Which City is the capital of India</h2>
+        <ul>
+            <li>Kolkata</li>
+            <li>Mumbai</li>
+            <li>Delhi</li>
+            <li>Bengaluru</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 questions</div>
+    </div>
   )
 }
 
